d7dfdf3e17b20899c635e9412fa3dc41140e3b15
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-07-18 13:24:11 +0000

    initial commit

---
+++ twitter_intro.py
@@ -0,0 +1,8 @@
+import json
+import tweepy
+from tweepy import OAuthHandler
+
+CONSUMER_KEY = 'gujUWVtlofQXqvjb6NXh1YHIa'
+CONSUMER_SECRET = 'TVzXFjFSmaphOBJiEflNJqYb2eYB6fx2I6LtYFhtpAGiLqA93j'
+OAUTH_TOKEN = '37690003-fWlxS1GiHpzXB3yhL2MrMfXTMhLHAOqqJegzOPKV7'
+OAUTH_TOKEN_SECRET = 'pUlDp3NlQqVzzXPuCNLqrI3xIuaQt8aPcyw6op0t0Aanz'
